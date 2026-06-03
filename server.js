8e81a5b03ba421180ea12be3628469678121537f
Ultron <talhaprince09@gmail.com>
2026-06-03 20:46:08 +0300

    Prepare backend for deployment

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL,
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
